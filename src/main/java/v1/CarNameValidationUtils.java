cbcdc6e2f0028804093d03b5b1e5d418b8c4b0f9
HanseongBae <bha4388@naver.com>
2024-03-21 20:32:22 +0900

    feat : 자동차 이름 검증 객체 기능 구현 및 테스트

---
+++ src/main/java/v1/CarNameValidationUtils.java
@@ -0,0 +1,23 @@
+package v1;
+
+public class CarNameValidationUtils {
+
+    private static final int LENGTH_LIMIT = 5;
+
+    public static void validateCarNames(final String[] carNames) {
+        for (String carName : carNames) {
+            validateCarName(carName);
+        }
+    }
+
+    private static void validateCarName(final String carName) {
+        if (isValid(carName)) {
+            throw new IllegalArgumentException("자동차의 이름은 한글자 이상 다섯글자 이하여야 합니다.");
+        }
+    }
+
+    private static boolean isValid(final String carName) {
+        return carName.length() == 0 || LENGTH_LIMIT < carName.length();
+    }
+
+}
