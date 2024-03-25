a3ccad303f96237833c00bee98392086fd66a42b
HanseongBae <bha4388@naver.com>
2024-03-26 07:56:49 +0900

    feat : 자동차의 위치상태를 가지는 불변객체 Position 구현 및 테스트

---
+++ src/main/java/v2/Position.java
@@ -0,0 +1,39 @@
+package v2;
+
+import java.util.Objects;
+
+public class Position {
+    private final int position;
+
+    public Position(final int position) {
+        validatePosition(position);
+        this.position = position;
+    }
+
+    public Position move(final int fuel) {
+        if (fuel >= 4) {
+            return new Position(position + 1);
+        }
+        return this;
+    }
+
+    private void validatePosition(final int position) {
+        if (position < 0) {
+            throw new IllegalArgumentException("0 이상의 상태를 가져야합니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position1 = (Position) o;
+        return position == position1.position;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position);
+    }
+
+}
