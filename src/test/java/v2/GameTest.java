0f4658067b88cfe380f3ef21e0c217a74d259ef4
HanseongBae <bha4388@naver.com>
2024-03-28 07:48:28 +0900

    test : 테스트 코드 추가

--- src/test/java/v2/GameTest.java
+++ src/test/java/v2/GameTest.java
@@ -2,6 +2,8 @@ package v2;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
@@ -17,5 +19,31 @@ public class GameTest {
 
         // then
         assertThat(end).isTrue();
+    }
+
+    @Test
+    void 레이싱게임_객체는_문자열로_자동차들을_등록한다() {
+        // given
+        Game game = new Game("dog,cat", 1);
+
+        // when
+        final List<Car> currentCars = game.getCurrentCars();
+
+        // then
+        assertThat(currentCars).hasSize(2)
+                .contains(new Car("dog"), new Car("cat"));
+    }
+
+    @Test
+    void 레이싱게임_객체는_우승자_선별을_일급콜렉션에_위임한다() {
+        // given
+        Game game = new Game("dog,cat,eagle", 1);
+
+        // when
+        final List<Car> winner = game.getWinners();
+
+        // then
+        assertThat(winner).hasSize(3)
+                .contains(new Car("dog"), new Car("cat"), new Car("eagle"));
     }
 }
